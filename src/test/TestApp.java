adb8d21b30248aa8ea8201f30afa715769acc95d
Imnotcoderdude <topsolo7@naver.com>
2024-05-03 10:21:14 +0900

    원의 넓이 연산 추가. 주석 기입 끝

--- src/test/TestApp.java
+++ src/test/TestApp.java
@@ -10,38 +10,44 @@ import java.util.Scanner;
 
 public class TestApp {
     public static void main(String[] args) {
+        // 사용자로부터 값을 입력받기 위해 Scanner 사용
         Scanner sc = new Scanner(System.in);
+        // TestCalculator의 객체를 생성. 계산해야하는 것이 사칙연산과 원의 넓이 두가지 이므로 배열 두개 설정
         TestCalculator calculator = new TestCalculator(new ArrayList<>(), new ArrayList<>());
 
-
+        // do - while 반복문을 사용하여 계산의 반복을 생성
         do {
-
             System.out.println("어떤 계산? 1.사칙연산 2.원의넓이");
+            // choice로 숫자를 입력받아서 사칙연산, 원의넓이 둘중 하나 선택
             int choice = sc.nextInt();
-
+            // choice로 받은 숫자가 1일 경우 사칙연산 수행
             if (choice == 1) {
-
-
+                // 정수와 연산기호를 입력받는 부분
                 System.out.print("첫 번째 숫자를 입력하세요 : ");
                 int num1 = sc.nextInt();
                 System.out.print("두 번째 숫자를 입력하세요 : ");
                 int num2 = sc.nextInt();
                 System.out.print("연산기호를 입력하세요 : ");
                 char oper = sc.next().charAt(0);
-
+                // 입력받은 값을 객체에서 연산하고 결과를 가져오는 부분.
                 int result = calculator.calculate(num1, num2, oper);
+                // 연산된 결과를 배열 안에 저장하는 부분.
                 calculator.getResults().add(result);
 
-
+                // 인덱스 삭제 로직
                 System.out.println("배열에 저장된 0번째 인덱스를 삭제하시겠습니까? (remove)");
+                // sc.next로 입력받은 값이 remove일 경우 실행, remve 말고 다른 값 입력시 아래로 넘어감
                 if (Objects.equals(sc.next(), "remove")) {
+                    // 계산기 내부에 0번째 인덱스를 삭제한다.
                     calculator.removeResult(0);
                 }
+                // inquiry를 입력받을시 작동, 저장된 모든 결과를 출력한다.
                 System.out.println("저장된 연산 결과 조회 (inquiry)");
                 if (Objects.equals(sc.next(), "inquiry")) {
                     calculator.inquiryResults();
                 }
             }else {
+                // 원의 넓이를 계산하고 출력하는 부분.
                 System.out.println("원의 반지름 : ");
                 int radius = sc.nextInt();
 
@@ -52,7 +58,9 @@ public class TestApp {
                 System.out.println("저장된 원의 넓이 전체 조회");
                 calculator.inquiryCircleresult();
             }
+            // 두 와일 문의 와일 부분, exit를 입력받을 경우 종료한다.
             System.out.println("더 계산하시겠습니까? (exit 입력시 종료) ");
+            // exit 외에 다른 값을 입력받을경우 다시 do 부분으로 돌아가서 계산을 다시 수행힌다.
         } while (!sc.next().equals("exit"));
     }
 }
--- src/test/TestCalculator.java
+++ src/test/TestCalculator.java
@@ -5,35 +5,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestCalculator {
-
+    // 사칙연산 계산 수행을 위해 Integer 타입의 리스트 필드 선언
     private List<Integer> results;
+    // 원의 넓이 계산 수행을 위해 Double 타입의 리스트 필드 선언
     private List<Double> circuleresult;
 
+    // 생성자, 두개의 리스트를 매개변수로 활용한다.
     public TestCalculator(List<Integer> results, List<Double> circuleresult) {
         this.results = results;
         this.circuleresult = circuleresult;
     }
-
+    // 사칙연산을 수행하는 메서드
     public int calculate(int num1, int num2, char oper) {
         int result = 0;
-
+        // 스위치문을 활용하여 oper의 값에 따라 맞는 연산을 제공한다.
         switch (oper) {
             case '+':
                 result = num1 + num2;
+                break;
             case '-':
                 result = num1 - num2;
+                break;
             case '*':
                 result = num1 * num2;
+                break;
             case '/':
                 if (num2 != 0) {
                     result = num1 / num2;
                 } else {
+                    // 나눗셈에서 분모에 0이 올 경우 예외처리한다.
                     throw new ArithmeticException("분모에는 0이 올 수 없습니다.");
                 }
                 break;
             default:
+                // 연산 기호를 제대로 입력하지 않을 경우 예외처리를 한다.
                 throw new UnsupportedOperationException("정확한 연산 기호를 입력해 주세요");
         }
+        // 위 스위치 문에서 나온 결과값을 출력하고 리턴한다.
+        System.out.println("연산 결과는 : " + result);
         return result;
     }
 
@@ -46,29 +55,29 @@ public class TestCalculator {
     public double calculateCircleArea(int radius) {
         return 3.14 * radius * radius;
     }
-
+    // 프라이빗된 result를 사용하기 위한 getter
     public List<Integer> getResults() {
         return results;
     }
-
+    // 프라이빗된 result를 사용하기 위한 setter
     public void setResults(List<Integer> results) {
         this.results = results;
     }
-
+    // remove를 입력받을 경우 인덱스를 학제하기 위한 메서드
     public void removeResult(int index) {
         this.results.remove(index);
     }
-
+    // inquiry를 입력받을 경우 저장된 인덱스를 전부 출력하기 위한 메서드
     public void inquiryResults() {
         for (Integer result : results) {
             System.out.println("저장된 연산 결과: " + result);
         }
     }
-
+    // 원의 넓이 getter
     public List<Double> getCirculeresult() {
         return circuleresult;
     }
-
+    // 원의 넓이 setter
     public void setCirculeresult(List<Double> circuleresult) {
         this.circuleresult = circuleresult;
     }
